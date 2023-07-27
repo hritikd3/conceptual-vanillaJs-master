876555f94959290b0d580b2f23b34b240f9fc3ff
hritikd3 <hritikdangi3@gmail.com>
2023-07-27 10:36:04 +0530

    example of spread operator

--- spreadOperator.js
+++ spreadOperator.js
@@ -1,2 +1,3 @@
-const arr=[1,2,3,4]
-console.log(...arr, "abc")
+// whenever we want to use the value of array individually we use spread operator
+const arr = [1, 2, 3, 4];
+console.log(...arr, "abc");
